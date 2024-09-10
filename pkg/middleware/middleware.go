d02bf034f0cdb8665e37b6cc095b9a9f725be2ff
alexdenkk <alexdenkk@alexdenkk.my.domain>
2024-09-11 02:58:43 +0700

    Код ревью епт

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -9,3 +9,17 @@ func New(key []byte) *Middleware {
 		Signkey: key,
 	}
 }
+
+/*
+Добавь middleware для авторизации
+
+type HandlerFunc func(w http.ResponseWriter, r *http.Request, act *token.Claims)
+
+func (m *Middleware) Auth(f HandlerFunc) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Тут получение токена из кукисов
+		// и его проверка
+		f(w, r, act)
+	}
+}
+*/
